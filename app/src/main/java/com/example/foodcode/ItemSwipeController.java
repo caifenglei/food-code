9eb51b14058756a76cdd542b038a7fa22843388a
caifenglei <caifenglei@huocheetc.com>
2022-09-01 15:42:50 +0800

    onChildDraw

--- app/src/main/java/com/example/foodcode/ItemSwipeController.java
+++ app/src/main/java/com/example/foodcode/ItemSwipeController.java
@@ -5,23 +5,122 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import static androidx.recyclerview.widget.ItemTouchHelper.*;
 
-public class ItemSwipeController extends ItemTouchHelper.Callback{
+import android.annotation.SuppressLint;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.View;
+
+enum SwipeState {GONE, RIGHT_VISIBLE}
+
+public class ItemSwipeController extends ItemTouchHelper.Callback {
+
+    // define is action is swipe back
+    private Boolean swipeBack = false;
+    private static final float refundButtonWidth = 84;
+    private SwipeState swipeShownState = SwipeState.GONE;
+
 
     //tells helper what kind of actions RecyclerView should handle
     @Override
-    public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder){
+    public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
         return makeMovementFlags(0, LEFT);
     }
 
     //what to do on given actions: move
     @Override
-    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target){
+    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
         return false;
     }
 
     //what to do on given actions: swipe
     @Override
-    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction){
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
 
+    }
+
+    // block swipe somehow
+    @Override
+    public int convertToAbsoluteDirection(int flags, int layoutDirection) {
+        if (swipeBack) {
+            swipeBack = false;
+            return 0;
+        }
+        return super.convertToAbsoluteDirection(flags, layoutDirection);
+    }
+
+    // draw child event handler
+    @Override
+    public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        if (actionState == ACTION_STATE_SWIPE) {
+            setTouchListener(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void setTouchListener(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        recyclerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                swipeBack = motionEvent.getAction() == MotionEvent.ACTION_CANCEL || motionEvent.getAction() == MotionEvent.ACTION_UP;
+                if (swipeBack) {
+                    //move enough to set shown state
+                    if (dX < -refundButtonWidth) {
+                        swipeShownState = SwipeState.RIGHT_VISIBLE;
+                    }
+
+                    // swipe visible
+                    if (swipeShownState != SwipeState.GONE) {
+                        setTouchDownListener(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+                        setItemsClickable(recyclerView, false);
+                    }
+                }
+                return false;
+            }
+        });
+    }
+
+    //simulate click on RecyclerView
+    @SuppressLint("ClickableViewAccessibility")
+    private void setTouchDownListener(final Canvas c, final RecyclerView recyclerView, final RecyclerView.ViewHolder viewHolder, final float dX, final float dY, final int actionState, final boolean isCurrentActive) {
+
+        recyclerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    setTouchUpListener(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentActive);
+                }
+                return false;
+            }
+        });
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void setTouchUpListener(final Canvas c, final RecyclerView recyclerView, final RecyclerView.ViewHolder viewHolder, final float dX, final float dY, final int actionState, final boolean isCurrentlyActive) {
+        recyclerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    ItemSwipeController.super.onChildDraw(c, recyclerView, viewHolder, 0F, dY, actionState, isCurrentlyActive);
+                    recyclerView.setOnTouchListener(new View.OnTouchListener() {
+                        @Override
+                        public boolean onTouch(View v, MotionEvent event) {
+                            return false;
+                        }
+                    });
+                    setItemsClickable(recyclerView, true);
+                    swipeBack = false;
+                    swipeShownState = SwipeState.GONE;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void setItemsClickable(RecyclerView recyclerView, boolean isClickable){
+        for(int i = 0; i < recyclerView.getChildCount(); ++i){
+            recyclerView.getChildAt(i).setClickable(isClickable);
+        }
     }
 }
